e26f35b13376064ecc8a83baab72978b11f38c9b
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-11-23 16:04:53 -0500

    Se inlcuyo un for con el cual se inicia la matriz y varias librerias con las cuales se va a llenar la matriz de numeros aleatorios

--- Ejercicio.c
+++ Ejercicio.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
 int n,m;
@@ -7,5 +9,17 @@ scanf("%d", &n);
 printf("Coloque los datos de m\n");
 scanf("%d", &m);
 int matrix[n][m];
+for (int i = 0; i < n; i++)
+{
+    printf("\n");
+
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i][j]=rand()%101;
+        printf("%d \t", matrix[i][j]);
+    }
+    
+}
+
 
 }
